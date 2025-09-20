fc1e20888300d79a750f1063453e81d3dc528c24
Domini <dominika.pudilova@gmail.com>
2025-09-20 21:11:19 +0200

    2025-09-20 vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/softui.css', 'resources/css/bs.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/softui.css'],
             refresh: true,
         }),
     ],
